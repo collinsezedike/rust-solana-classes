b125c759c82e097520dc0a0a98360ed8c873afed
yuhcee <egbouchenna001@gmail.com>
2025-06-26 14:58:09 +0100

    wrote function greeter

--- assignments/ep01_intro_to_rust/yuhcee/src/main.rs
+++ assignments/ep01_intro_to_rust/yuhcee/src/main.rs
@@ -2,5 +2,15 @@ use std::io;
 
 fn main() {
     println!("Hello, Rusteaceans!");
+    greeter();
 }
 
+fn greeter() {
+    println!("What is your name?");
+    let mut name = String::new();
+    io::stdin()
+    .read_line(&mut name)
+    .expect("Failed to read input");
+
+    println!("Welcome to Rust, {}!", name.trim());
+}
