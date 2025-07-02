1993fe0cec792269a20a4409f2be5e7286d4772d
yuhcee <egbouchenna001@gmail.com>
2025-07-02 23:01:47 +0100

    feat: Move arithmetic functions to a dedicated module and update lib.rs

--- assignments/ep01_intro_to_rust/yuhcee/src/lib.rs
+++ assignments/ep01_intro_to_rust/yuhcee/src/lib.rs
@@ -1,20 +1,4 @@
-pub fn multiply_numbers(a: i32, b: i32) -> i32 {
-    a * b
-}
-
-pub fn add_numbers(a:i32, b:i32) -> i32 {
-    a + b
-}
-
-pub fn subtract_numbers(a:i32, b:i32) -> i32 {
-    a - b
-}
-
-pub fn divide_numbers(a:i32, b:i32) -> i32 {
-    if b == 0 {
-        panic!("cannot divide by zero");
-    }
-
-    a / b
-}
-
+pub mod fizz_buzz;
+pub mod interactive_menu;
+pub mod greeter;
+pub mod arithmetic;
